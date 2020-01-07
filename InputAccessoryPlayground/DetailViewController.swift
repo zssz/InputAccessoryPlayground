48d794102b9c860ddac9e871fbadf23293ef2cd7
Zsombor Szabo <zsombor@gmail.com>
2020-01-07 12:04:56 +0200

    Initial Commit

---
+++ InputAccessoryPlayground/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Created by Zsombor Szabo on 07/01/2020.
+//  Copyright © 2020 IZE. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
